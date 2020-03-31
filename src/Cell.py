19e9294ecefe3fa451b2991fcf30afc179aaf87d
LeiYangGH <675686066@qq.com>
2020-03-31 23:32:09 +0800

    doing compare possibles

--- src/Cell.py
+++ src/Cell.py
@@ -28,7 +28,28 @@ class cell:
 
     def getPossibles(self, snapshot):
         if not self.possibles:
-            self.possibles = {1, 2, 3, 4, 5} \
+            fullset = {1, 2, 3, 4, 5}
+            for (coords1, coords2) in snapshot.getConstraints():
+                if coords1[0] == self.row and coords1[1] == self.col: #self is lower
+                    var2 = snapshot.getCellVal(coords2[0], coords2[1])
+                    print(f'var2={var2}')
+                    for small in range(1, var2):
+                        if small in fullset:
+                            fullset.remove(small)
+                            print(f'remove small {small}')
+                if coords2[0] == self.row and coords2[1] == self.col: #self is bigger
+                    var1 = snapshot.getCellVal(coords1[0], coords1[1])
+                    print(f'var1={var1}')
+
+                    for big in range(var1,6):
+                        if big in fullset:
+                            fullset.remove(big)
+                            print(f'remove big {big}')
+
+
+                # var1 = snapshot.getCellVal(coords1[0], coords1[1])
+                # var2 = snapshot.getCellVal(coords2[0], coords2[1])
+            self.possibles = fullset \
                              - set([c.val for c in snapshot.cellsByRow(self.row)]) \
                              - set([c.val for c in snapshot.cellsByCol(self.col)])
         print(f'self.possibles={self.possibles}')
